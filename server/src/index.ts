b6f1e6d4cfe8bceb181884cb7325e96606bfa829
aakashmangla2000 <akkibaby90@yahoo.com>
2023-03-24 16:03:14 +0530

    Add route to get specific book

--- server/src/index.ts
+++ server/src/index.ts
@@ -27,11 +27,45 @@ app.get("/api/books", async (req: Request, res: Response) => {
   }
 });
 
-// app.get("/api/book/:bookId", (req: Request, res: Response) => {
-//   res.status(200).json({
-//     status: "success",
-//     data: { book: req.params },
-//   });
+app.get("/api/book/:bookId", async (req: Request, res: Response) => {
+  try {
+    const results = await pool.query(
+      `select books.id,books.name as book_name,array_agg(authors.name) as authors,details,time_to_read from books inner join books_authors on books.id = books_authors.book_id inner join authors on books_authors.author_id = authors.id
+where books.id = ${req.params.bookId} group by books.id;`
+    );
+    res.status(200).json({
+      status: "success",
+      results: results.rows.length,
+      data: results.rows,
+    });
+  } catch (err) {
+    console.log(err);
+  }
+  console.log(req.params);
+});
+
+// app.post("/api/books", async (req: Request, res: Response) => {
+//   //   res.status(200).json({
+//   //     status: "success",
+//   //     data: { book: req.body },
+//   //   });
+//   //   console.log(req.body);
+
+//   try {
+//     const { item, price, record_time } = req.body;
+//     const newTrans = await pool.query(
+//       "INSERT INTO transactions (item, price, record_time) VALUES ($1, $2, $3) RETURNING *",
+//       [item, price, record_time]
+//     );
+//     res.status(200).json({
+//       status: "success",
+//       data: { book: req.body },
+//     });
+//     console.log(req.body);
+//     // res.json(newTrans.rows[0]);
+//   } catch (error) {
+//     console.log(error);
+//   }
 // });
 
 app.listen(port, () => {
