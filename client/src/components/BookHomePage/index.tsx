2325483a61db3a83b5a1b599ca665cfb841ed736
aakashmangla2000 <akkibaby90@yahoo.com>
2023-03-24 19:24:40 +0530

    Add project structure, home page with nav bar and books data from get API

---
+++ client/src/components/BookHomePage/index.tsx
@@ -0,0 +1,38 @@
+import * as React from "react";
+import Card from "@mui/material/Card";
+import CardContent from "@mui/material/CardContent";
+import CardMedia from "@mui/material/CardMedia";
+import Typography from "@mui/material/Typography";
+import { CardActionArea } from "@mui/material";
+
+type HomePageBookProps = {
+  bookName: string;
+  authors: string[];
+};
+
+export default function BookHomePage(props: HomePageBookProps) {
+  return (
+    <Card sx={{ maxWidth: 345, margin: "20px" }}>
+      <CardActionArea
+        onClick={() => {
+          console.log(props.bookName);
+        }}
+      >
+        <CardMedia
+          component="img"
+          height="340"
+          image={require("./HarryPotter.jpg")}
+          alt="Book Cover"
+        />
+      </CardActionArea>
+      <CardContent>
+        <Typography gutterBottom variant="h5" component="div">
+          {props.bookName}
+        </Typography>
+        <Typography variant="body2" color="text.secondary">
+          {props.authors.map((author: string) => `${author}, `)}
+        </Typography>
+      </CardContent>
+    </Card>
+  );
+}
---
+++ client/src/components/Home/index.tsx
@@ -0,0 +1,63 @@
+import { type } from "os";
+import React, { useEffect, useState } from "react";
+import BookHomePage from "../BookHomePage";
+import MenuBookIcon from "@mui/icons-material/MenuBook";
+
+type Book = {
+  id: number;
+  book_name: string;
+  details: string;
+  time_to_read: number;
+  authors: Array<string>;
+};
+
+function Home() {
+  const [books, setBooks] = useState<Book[] | null>(null);
+
+  const getBooksData = async () => {
+    try {
+      await fetch(`http://localhost:3001/api/books`)
+        .then((res) => res.json())
+        .then((data) => {
+          console.log(data.data);
+          setBooks(data.data);
+        });
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
+  useEffect(() => {
+    getBooksData();
+  }, []);
+
+  return (
+    <div style={{ padding: "30px" }}>
+      <div
+        style={{
+          width: "100%",
+          display: "flex",
+          alignItems: "flex-start",
+          paddingBottom: "30px",
+          paddingLeft: "20px",
+        }}
+      >
+        <MenuBookIcon style={{ fontSize: "50px" }} />
+        <span style={{ fontSize: "50px" }}>My Books</span>
+      </div>
+      <div style={{ display: "flex" }}>
+        {books !== null
+          ? books.map((book) => (
+              <BookHomePage
+                key={book.id}
+                bookName={book.book_name}
+                authors={book.authors}
+              />
+            ))
+          : ""}
+      </div>
+    </div>
+  );
+}
+
+export default Home;
